d6af73add6e00b46e81ce34cac9342472b7895dc
teemupuuska <teemu.puuska@hotmail.com>
2016-05-08 23:52:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Teemu? Just say HELLO to get started.')
+            return bot.say('Hi there! My name is Albert, so you want to learn about Teemu? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
